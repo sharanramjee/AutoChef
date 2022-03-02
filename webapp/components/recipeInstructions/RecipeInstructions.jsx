fa3cdd209d2c489afe891f9cdffb6f63c2f8c7a0
sharanramjee <sharanramjee@gmail.com>
2022-03-02 10:44:56 -0800

    Added instructions successfully to RecipeInstructions.jsx

--- webapp/components/recipeInstructions/RecipeInstructions.jsx
+++ webapp/components/recipeInstructions/RecipeInstructions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Divider, Typography} from '@material-ui/core';
+import {Button, Divider, List, ListItem, ListItemText, Typography} from '@material-ui/core';
 import './RecipeInstructions.css';
 import axios from 'axios';
 
@@ -14,6 +14,7 @@ class RecipeInstructions extends React.Component {
       instructions: undefined,
     }
     this.queryStringify = this.queryStringify.bind(this);
+    this.instructionsList = this.instructionsList.bind(this);
   }
 
   queryStringify(breakdown, recipe_id) {
@@ -26,8 +27,8 @@ class RecipeInstructions extends React.Component {
     let query_string = this.queryStringify(true, this.state.recipe_id);
     axios.get('http://127.0.0.1:5000/insts?' + query_string)
     .then(response => {
-      console.log('NAENAE:', response)
-      this.setState({instructions: response.data});
+      console.log('NAENAE:', response.data[0].instructions[0].steps)
+      this.setState({instructions: response.data[0].instructions[0].steps});
     })
     .catch(err => {
       console.log(err.response);
@@ -48,37 +49,34 @@ class RecipeInstructions extends React.Component {
 //     }
 //   }
 
+  instructionsList() {
+    let instructions_list = [];
+    for(let i = 0; i < this.state.instructions.length; i++){
+      instructions_list.push(
+        <div key = {i}>
+          <ListItem>
+            <ListItemText className='recipe-step' primary={(i+1).toString() + '. ' + this.state.instructions[i].step}/>
+          </ListItem>
+          {/* <Divider /> */}
+        </div>
+      );
+    }
+    return instructions_list;
+  }
+
   render() {
     console.log(this.state.instructions);
     if (this.state.instructions) {
       return (
         <div>
           <Typography variant='h2'>
-            {this.state.user.first_name + ' ' + this.state.user.last_name}
+            Instructions:
             <br/>
           </Typography>
-          <Typography variant='subtitle1'>
-            &quot;{this.state.user.description}&quot;
-            <br/>
-            <strong>Location: </strong> {this.state.user.location}
-            <br/>
-            <strong>Occupation: </strong> {this.state.user.occupation}
-            <br/><br/>
-          </Typography>
-          {/* <Button variant='contained' color='primary' className='cs142-userDetail-button' component='a'
-            href = {'#/photos/' + this.state.user._id}>
-            Photos
-          </Button>
-          <br/> <br/> <Divider/>
-          <Typography variant='h5'>Mentioned in:</Typography>
-          <br/>
-          {this.state.user.mentioned.length >= 1 ? (
-            this.state.user.mentioned.map((photo_id, i) => {
-              return <Mentions key={photo_id + i} curr_user_id={this.props.curr_user_id} photo_id={photo_id} />;
-            })
-          ) : (
-            <Typography>No mentions</Typography>
-          )} */}
+          <List component='recipe-steps'>
+            <Divider/>
+            {this.instructionsList()}
+          </List>
         </div>
       );
     }
